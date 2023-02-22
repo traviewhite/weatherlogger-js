089c7e2e3468a00a080ec743b80f1010f3fb9c6c
Travis White <39642201+traviewhite@users.noreply.github.com>
2023-02-21 21:15:40 -0600

    test: remove temp

--- src/getWeatherData.ts
+++ src/getWeatherData.ts
@@ -6,7 +6,7 @@ export default async function () {
   const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_KEY}&units=${unit}`);
   return {
       time: moment().format('YYYY-MM-DD HH:mm:ss'),
-      temp: weather.data.main.temp,
+      // temp: weather.data.main.temp,
       wind: weather.data.wind.speed
   }
 }
